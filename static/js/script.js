bb7f344b8943d67fbc4920ead4496364e26343a9
Aicha-khorchani <aichakhorchani47@gmail.com>
2025-01-12 17:00:58 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-let menuIcon = document.querySelector('menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let selections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
